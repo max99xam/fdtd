1b259ecd50ba867da81920d5ce1895a5f535312e
max99xam <pepetrov123@list.ru>
2022-02-26 18:11:11 +0300

    added animation

--- fdtd-1D-1-1.py
+++ fdtd-1D-1-1.py
@@ -1,63 +1,84 @@
 """
 fdtd-1D-1-1.py
-Simulation in free space.
+Simulation in free space (without boundary condition).
 """
 import numpy as np
 from math import exp
 from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation
 
-width = 250
+# Work area width.
+width = 150
+
+# Data.
 ex = np.zeros(width)
 hy = np.zeros(width)
+x = np.arange(0, width)
 
 # Pulse parameters
-kc = int(width / 2)
+source_position = int(width / 2)
 t0 = 40
 spread = 12
 
-nsteps = 100
+max_iterations = 1200
 
-# Main FDTD Loop
-for time_step in range(1, nsteps + 1):
-    # Calculate the Ex field
+# Plot configuration.
+fig, (ax1, ax2) = plt.subplots(2)
+
+x_begin = 0
+x_end = width
+y_begin = -2.0
+y_end = 2.0
+
+font_size = 14
+plt.rcParams['font.size'] = font_size
+
+ax1.set_xlim(x_begin, x_end)
+ax1.set_ylim(y_begin, y_end)
+ax1.set_ylabel('E$_x$', fontsize=font_size)
+
+ax2.set_xlim(x_begin, x_end)
+ax2.set_ylim(y_begin, y_end)
+ax2.set_ylabel('H$_y$', fontsize=font_size)
+
+line1, = ax1.plot([], [], lw=2)
+line2, = ax2.plot([], [], lw=2)
+
+
+def init():
+    line1.set_data(x, ex)
+    line2.set_data(x, ex)
+    return line1, line2
+
+
+def update(iteration_step):
+    # Calculate the Ex field.
     for k in range(1, width):
         ex[k] = ex[k] + 0.5 * (hy[k - 1] - hy[k])
 
     # Electromagnetic "hard" source.
-    # Put a Gaussian pulse in the middle
-    pulse = exp(-0.5 * ((t0 - time_step) / spread) ** 2)
+    # Put a Gaussian pulse in the middle.
+    pulse = exp(-0.5 * ((t0 - iteration_step) / spread) ** 2)
 
-    source_dist = 50
+    source_dist = 20
 
     # Two sources.
-    ex[kc-source_dist] = pulse
-    ex[kc+source_dist] = pulse
+    ex[source_position - source_dist] = pulse
+    ex[source_position + source_dist] = pulse
 
-    # Calculate the Hy field
+    # Calculate the Hy field.
     for k in range(width - 1):
         hy[k] = hy[k] + 0.5 * (ex[k] - ex[k + 1])
 
-# Plot Ex
-plt.rcParams['font.size'] = 12
-plt.figure(figsize=(8, 3.5))
-plt.subplot(211)
-plt.plot(ex, color='red', linewidth=1)
-plt.ylabel('E$_x$', fontsize='14')
-plt.xticks(np.arange(0, width+1, step=20))
-plt.xlim(0, width)
-plt.yticks(np.arange(-1, 1.2, step=1))
-plt.ylim(-1.2, 1.2)
-plt.text(width/2, 1.5, 'Time step = {}'.format(time_step), horizontalalignment='center')
+    # Update plot data.
+    line1.set_data(x, ex)
+    line2.set_data(x, hy)
+    title = ax1.text(width / 2, 1.45, 'Iteration step = {}'.format(iteration_step), horizontalalignment='center')
 
-# Plot Hy
-plt.subplot(212)
-plt.plot(hy, color='k', linewidth=1)
-plt.ylabel('H$_y$', fontsize='14')
-plt.xlabel('FDTD cells')
-plt.xticks(np.arange(0, width+1, step=20))
-plt.xlim(0, width)
-plt.yticks(np.arange(-1, 1.2, step=1))
-plt.ylim(-1.2, 1.2)
+    return line1, line2, title
 
-plt.subplots_adjust(bottom=0.2, hspace=0.45)
+
+anim = FuncAnimation(fig, update, init_func=init,
+                     frames=max_iterations, interval=20, blit=True)
+
 plt.show()
--- main.py
+++ main.py
@@ -1,27 +1,83 @@
+"""
+Script for test.
+"""
 import numpy as np
+from math import exp
 from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-plt.style.use('seaborn-pastel')
+# Work area width.
+width = 150
 
-fig = plt.figure()
-ax = plt.axes(xlim=(0, 4), ylim=(-2, 2))
-line, = ax.plot([], [], lw=3)
+# Data.
+ex = np.zeros(width)
+hy = np.zeros(width)
+x = np.arange(0, width)
+
+# Pulse parameters
+source_position = int(width / 2)
+t0 = 40
+spread = 12
+
+max_iterations = 1200
+
+# Plot configuration.
+fig, (ax1, ax2) = plt.subplots(2)
+
+x_begin = 0
+x_end = width
+y_begin = -2.0
+y_end = 2.0
+
+font_size = 14
+plt.rcParams['font.size'] = font_size
+
+ax1.set_xlim(x_begin, x_end)
+ax1.set_ylim(y_begin, y_end)
+ax1.set_ylabel('E$_x$', fontsize=font_size)
+
+ax2.set_xlim(x_begin, x_end)
+ax2.set_ylim(y_begin, y_end)
+ax2.set_ylabel('H$_y$', fontsize=font_size)
+
+line1, = ax1.plot([], [], lw=2)
+line2, = ax2.plot([], [], lw=2)
 
 
 def init():
-    line.set_data([], [])
-    return line,
+    line1.set_data(x, ex)
+    line2.set_data(x, ex)
+    return line1, line2
 
 
-def update(i):
-    x = np.linspace(0, 4, 1000)
-    y = np.sin(2 * np.pi * (x - 0.01 * i))
-    line.set_data(x, y)
-    return line,
+def update(iteration_step):
+    # Calculate the Ex field.
+    for k in range(1, width):
+        ex[k] = ex[k] + 0.5 * (hy[k - 1] - hy[k])
+
+    # Electromagnetic "hard" source.
+    # Put a Gaussian pulse in the middle.
+    pulse = exp(-0.5 * ((t0 - iteration_step) / spread) ** 2)
+
+    source_dist = 20
+
+    # Two sources.
+    ex[source_position - source_dist] = pulse
+    ex[source_position + source_dist] = pulse
+
+    # Calculate the Hy field.
+    for k in range(width - 1):
+        hy[k] = hy[k] + 0.5 * (ex[k] - ex[k + 1])
+
+    # Update plot data.
+    line1.set_data(x, ex)
+    line2.set_data(x, hy)
+    title = ax1.text(width / 2, 1.45, 'Iteration step = {}'.format(iteration_step), horizontalalignment='center')
+
+    return line1, line2, title
 
 
 anim = FuncAnimation(fig, update, init_func=init,
-                     frames=200, interval=20, blit=True)
+                     frames=max_iterations, interval=20, blit=True)
 
 plt.show()
