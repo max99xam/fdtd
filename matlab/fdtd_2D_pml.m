76a025b1d93045b3a648e1b2f2ac8000099e1325
max99xam <pepetrov123@list.ru>
2022-04-30 20:29:50 +0300

    added 2D matlab with PML based on book code

---
+++ matlab/fdtd_2D_pml.m
@@ -0,0 +1,170 @@
+% 2D FDTD with PML.
+
+% Based on code from book:
+% Electromagnetic simulation using the fdtd method with python.
+% Chapter 3.
+
+% Grid sizes.
+rows = 200;
+cols = 200;
+
+% Set source position
+source_position_row = 150;
+source_position_col = 82;
+
+% Light speed.
+c0 = 2.99792458e8;
+
+% Permeability of free space.
+mu0 = 4.0 * pi * 1.0e-7;
+
+% Permittivity.
+epsilon0 = 1;
+epsilon1 = 1;
+epsilon2 = 1;
+epsz = 8.8e-12;
+
+
+% Conductivity.
+sigma0 = 5.7e8;
+sigma1 = 0.001;
+sigma2 = 0.001;
+
+dz = zeros(rows,cols);
+ez = zeros(rows,cols);
+hx = zeros(rows,cols);
+hy = zeros(rows,cols);
+
+gaz = ones(rows,cols);
+
+% Max simulation time.
+max_time = 1000;
+
+% Space grid step.
+ddx = 1.0e-3;
+ddy = ddx;
+
+% Time step corressponding Courant factor.
+dt = .98 / (c0 * sqrt((1 / ddx)^2 + (1 / ddy)^2));
+
+% Source params.
+% Gaussian beam.
+t0 = 20;
+% Beam width.
+tau = 20;
+
+% PML params.
+gi2 = ones(rows);
+gi3 = ones(rows);
+fi1 = zeros(rows);
+fi2 = ones(rows);
+fi3 = ones(rows);
+
+gj2 = ones(rows);
+gj3 = ones(rows);
+fj1 = zeros(rows);
+fj2 = ones(rows);
+fj3 = ones(rows);
+
+% Calculate PML.
+pml_width = 50;
+for i = 1:pml_width
+    xnum = pml_width - i;
+    xd = pml_width;
+    xxn = xnum / xd;
+    xn = 0.33 * ((xxn)^3);
+
+    gi2(i) = 1.0 / (1.0 + xn);
+    gi2(rows - 1 - i) = 1.0 / (1.0 + xn);
+    gi3(i) = (1.0 - xn) / (1.0 + xn);
+    gi3(rows - i - 1) = (1.0 - xn) / (1.0 + xn);
+
+    gj2(i) = 1.0 / (1.0 + xn);
+    gj2(rows - 1 - i) = 1.0 / (1.0 + xn);
+    gj3(i) = (1.0 - xn) / (1.0 + xn);
+    gj3(rows - i - 1) = (1.0 - xn) / (1.0 + xn);
+
+    xxn = (xnum - 0.5) / xd;
+    xn = 0.25 * ((xxn)^3);
+
+    fi1(i) = xn;
+    fi1(rows - 2 - i) = xn;
+    fi2(i) = 1.0 / (1.0 + xn);
+    fi2(rows-2-i) = 1.0 / (1.0 + xn);
+    fi3(i) = (1.0 - xn) / (1.0 - xn);
+    fi3(rows - 2 - i) = (1.0 - xn) / (1.0 + xn);
+
+    fj1(i) = xn;
+    fj1(rows - 2 - i) = xn;
+    fj2(i) = 1.0 / (1.0 + xn);
+    fj2(rows-2-i) = 1.0 / (1.0 + xn);
+    fj3(i) = (1.0 - xn) / (1.0 - xn);
+    fj3(rows - 2 - i) = (1.0 - xn) / (1.0 + xn);
+end
+
+for i=100:180
+    for j=60:120
+        gaz(i,j)=1./(epsilon1+(sigma1*dt)/epsz);
+
+        % Make dielectric border.
+        if i>=120 && i<=160 && j>=74 && j<=75 ...
+        || i>=120 && i<=160 && j>=90 && j<=91 ...
+        || i>=160 && i<=161 && j>=75 && j<=130
+            gaz(i,j)=1./(epsilon0+(sigma0*dt)/epsz);
+        end
+    end
+end
+
+for i=70:80
+    for j=80:120
+        gaz(i,j)=1./(epsilon2+(sigma2*dt)/epsz);
+    end
+end
+
+% Main fdtd loop.
+% Time layers calculation.
+for time_step = 1:max_time
+
+    % Dz field calculation.
+    for j = 2:cols
+        for i = 2:rows
+            dz(i,j) = gi3(i) * gj3(j) * dz(i,j) ...
+                    + gi2(i) * gj2(j) * .5 ...
+                    * (hy(i,j) - hy(i-1,j) - hx(i,j) + hx(i,j-1));
+        end
+    end
+
+    % Ez field calculation.
+    for j = 2:cols
+        for i = 2:rows
+            ez(i,j) = gaz(i,j) * dz(i,j);
+        end
+    end
+
+    % Put Gaussian beam source.
+    source = -2.0 * ((time_step - t0) ./ tau) .* exp(-1.0 * ((time_step - t0) ./ tau) .^ 2.0);
+    ez(source_position_row, source_position_col) = source;
+
+    % Hx field calculation.
+    for j=1:cols-1
+        for i = 1:rows-1
+            hx(i,j) = fj3(j) * hx(i,j) + fj2(j) *0.5 * (ez(i,j) - ez(i,j+1));
+        end
+    end
+
+    % Hy field calculation.
+    for j = 1:cols-1
+        for i = 1:rows-1
+            hy(i,j) = fi3(i) * hy(i,j) + fi2(i) * 0.5 * (ez(i+1,j) - ez(i,j));
+        end
+    end
+
+    timestep=int2str(time_step);
+    imagesc(ez);
+    colormap(jet);
+    colorbar
+    title(['Ez at time step = ',timestep]);
+
+    pause(0.008);
+end
+
